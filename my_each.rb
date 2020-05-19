e80d482db2ce8d2e0d783aba9433187c6845ac14
Patricia Cordero <patricia@patriciaco.com>
2020-05-19 02:51:07 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,2 +1,18 @@
- .
+# def my_each(s) # put argument(s) here
+#    if block_given?
+#     i = 0
+#
+#      while i < array.size
+#       yield(array[i])
+#     i += 1
+#
+#     #array
+#
+#   end
+# end
+#
+#   collection = [].collect
+#   my_each(collection) do |i|
+#     puts word
+#   end
 end
