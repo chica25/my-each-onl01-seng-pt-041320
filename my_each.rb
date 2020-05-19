7dc66b7a082e7f28dc68572f9c06fa29cea723d8
Patricia Cordero <patricia@patriciaco.com>
2020-05-19 03:03:51 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,35 +1,35 @@
-# def my_each(s) # put argument(s) here
-#    if block_given?
-#     i = 0
-#
-#      while i < array.size
-#       yield(array[i])
-#     i += 1
-#
-#     #array
-#
-#   end
-# end
-#
-#   collection = [].collect
-#   my_each(collection) do |i|
-#     puts word
-#   end
-#end
+def my_each(s) # put argument(s) here
+   if block_given?
+    i = 0
+
+     while i < array.size
+      yield(array[i])
+    i += 1
+
+    #array
+
+  end
+end
+
+  collection = [].collect
+  my_each(collection) do |i|
+    puts word
+  end
+end
 
 
-# 
+#
 # def my_each(s)
 #   if block_given?
 #   i = 0
-# 
+#
 #   while i < array.size
 #     yield(array[i])
 #     i += 1
-# 
+#
 #   end
 # end
-# 
+#
 # collection = [1, 2, 3, 4]
 #   my_each(collection) do |i|
 #     puts
