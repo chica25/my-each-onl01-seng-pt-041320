5682cd59b18e36a838f71a6178824bbfc0cc768d
Patricia Cordero <patricia@patriciaco.com>
2020-05-19 03:03:48 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -18,20 +18,20 @@
 #end
 
 
-
-def my_each(s)
-  if block_given?
-  i = 0
-
-  while i < array.size
-    yield(array[i])
-    i += 1
-
-  end
-end
-
-collection = [1, 2, 3, 4]
-  my_each(collection) do |i|
-    puts
-    end
-  end
+# 
+# def my_each(s)
+#   if block_given?
+#   i = 0
+# 
+#   while i < array.size
+#     yield(array[i])
+#     i += 1
+# 
+#   end
+# end
+# 
+# collection = [1, 2, 3, 4]
+#   my_each(collection) do |i|
+#     puts
+#     end
+#   end
